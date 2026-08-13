770200942a15bc9a93e163341520047cec3c9bfe
Melissa <melissapmdasilva@gmail.com>
2026-08-12 22:49:32 -0300

    repositorio legal

---
+++ mes.pa/mes.pa/src/mes/pa/atividadedemespa.java
@@ -0,0 +1,58 @@
+package mes.pa;
+import java.util.Scanner;
+public class atividadedemespa {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int mesdoano;
+		
+		System.out.println("Insira um número");
+		mesdoano=scan.nextInt();
+
+		switch (mesdoano) {
+		case 1:
+			System.out.println("O mês é Janeiro");
+			break;
+		case 2:
+			System.out.println("O mês é fevereiro");
+			break;
+		case 3:
+			System.out.println("O mês é março");
+			break;
+		case 4:
+			System.out.println("O mês é abril");
+			break;
+		case 5:
+			System.out.println("O mês é maio");
+			break;
+		case 6:
+			System.out.println("O mês é junho");
+			break;
+		case 7:
+			System.out.println("O mês é julho");
+			break;
+		case 8:
+			System.out.println("O mês é agosto");
+			break;
+		case 9:
+			System.out.println("O mês é setembro");
+			break;
+		case 10:
+			System.out.println("O mês é outubro");
+			break;
+		case 11:
+			System.out.println("O mês é novembro");
+			break;
+		case 12:
+			System.out.println("O mês é dezembro");
+			break;
+			default:
+				System.out.println("Você inseriu um número que não é válido");
+				break;
+		}
+		scan.close();
+	}
+
+}
